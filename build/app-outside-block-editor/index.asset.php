07bfd575c87ca856abd28b2b4825f5cbc788302c
srtfisher <srtfisher@users.noreply.github.com>
2024-07-23 02:38:06 +0000

    Built changes for v0.1.1 from develop 0cf9c3e5e4adcb0cc7ba3aa3301dc4b8382a5c5e

---
+++ build/app-outside-block-editor/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-commands', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '00f8bb86a67f9b226a3c');
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alleyinteractive/wp-command-palette',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'alleyinteractive/wp-command-palette' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
