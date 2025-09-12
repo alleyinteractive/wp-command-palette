b6976229e026637f26269bb4cf53f08e569590eb
Sean Fisher <srtfisher@gmail.com>
2025-09-12 12:51:44 -0400

    Retain video

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,6 +26,8 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'http://localhost:8888',
 
+    video: 'retain-on-failure',
+
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
