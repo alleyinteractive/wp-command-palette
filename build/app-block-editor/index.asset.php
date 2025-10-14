c402571065b4ad499f2d512b6121c0937ed60a7a
srtfisher <srtfisher@users.noreply.github.com>
2025-10-14 12:53:09 +0000

    Built changes from develop 2d4be8e539cb191da1f45798cd117a0898ec502e

---
+++ build/app-block-editor/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-commands', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives'), 'version' => '4b7e6162eea05fa0edc2');
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alleyinteractive/wp-command-palette',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '2d4be8e539cb191da1f45798cd117a0898ec502e',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'alleyinteractive/wp-command-palette' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '2d4be8e539cb191da1f45798cd117a0898ec502e',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
